09dee36b36c7b817d5e07f79bac82187450d9cb7
Adrien Savoretti <adrien.savo@gmail.com>
2017-02-28 12:37:56 +0100

    Edit users.js (to require Json) and added utils.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+const utils = require('./utils');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   User.find().sort('name').exec(function(err, users) {
@@ -12,7 +14,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST new user */
-router.post('/', function(req, res, next) {
+router.post('/', utils.requireJson, function(req, res, next) {
   // Create a new document from the JSON in the request body
   const newUser = new User(req.body);
   // Save that document
