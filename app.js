412bbb6b48198c93c65cf7f46c7734f767b5d3ff
Adrien Savoretti <adrien.savo@gmail.com>
2017-03-01 16:06:23 +0100

    Add error handlers in app.js (to get errors in json or html), edit userValidationError in user.js

--- app.js
+++ app.js
@@ -42,13 +42,43 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+
+// API error handler (responds with JSON)
+app.use('/', function(err, req, res, next) {
+
+  // Log the error on stderr
+  console.warn(err);
+
+  // Respond with 422 Unprocessable Entity if it's a Mongoose validation error
+  if (err.name == 'ValidationError' && !err.status) {
+    err.status = 422;
+  }
+
+  // Set the response status code
+  res.status(err.status || 500);
+
+  // Send the error message in the response
+  const response = {
+    message: err.message
+  };
+
+  // If it's a validation error, also send the errors details from Mongoose
+  if (err.status == 422) {
+    response.errors = err.errors;
+  }
+
+  // Send the error response
+  res.send(response);
+});
+
+// Generic error handler (responds with HTML)
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
