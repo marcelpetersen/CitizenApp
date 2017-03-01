dfa6068a73d53826de367694d9d4a3fac116dc94
Adrien Savoretti <adrien.savo@gmail.com>
2017-03-01 15:44:50 +0100

    Edited users apidoc

--- docs/api_project.js
+++ docs/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "An app that encourages citizen participation",
   "title": "Citizen App",
-  "url": "https://heigvd-webserv-2017-team-7.herokuapp.com/",
+  "url": "https://heigvd-webserv-2017-team-7.herokuapp.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-03-01T13:47:35.344Z",
+    "time": "2017-03-01T14:08:53.638Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
